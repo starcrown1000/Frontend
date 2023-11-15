64e82cd1f3c99f349f288d98514162f79be674e7
janine <janinemoldin123@gmail.com>
2023-11-15 23:02:05 +0800

    ad files

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 function Main() {
   return (
-    <div className='bg-red-700 text-black min-h-screen'>Main</div>
+    <div className="hero min-h-screen bg-base-200">
+  <div className="hero-content text-center">
+    <div className="max-w-md">
+      <h1 className="text-5xl font-bold">Hi! I'm Janine</h1>
+      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <button className="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
   )
 }
 
